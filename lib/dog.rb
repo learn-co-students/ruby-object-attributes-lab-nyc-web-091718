6e36f5ada0461b0e3bf56a85fd0e7c48d7fa2363
Zeshawn <zeshawnk@gmail.com>
2018-09-18 14:58:28 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name= (dog_name)
+    @name = dog_name
+  end
+
+def breed
+  @breed
+end
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+end
+
+
+class Person
+  def name
+    @name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(jobs)
+    @job = jobs
+  end
+end
