05b46374a73014f41730167b77bfad15bdbf4795
Andrew Bernier <github email address>
2018-09-18 16:21:03 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+#attr_accessor :name, :breed (this replaces all of the code above)
+
+# def initialize (name, breed)
+  #@name = name
+  #@breed = breed
+#end
