c3a92239371b895a5241d305df706facd243fa1a
HannaStrezh <hannastrezh@gmail.com>
2018-11-05 13:58:31 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,72 @@
 require 'pry'
-
 class Dog
+      def name=(name)
+        @name= name
+      end
 
-  def name=(dogs_name)
-    @name= dogs_name
-end
+ def name
+   @name
+ end
 
-def name
-  @name
-end
+    def breed
+      @breed
+    end
 
-def breed= (breed)
-  @breed= breed
-end
+  def breed=(breed)
+    @breed = breed
+  end    
+end #end of a Dog class
 
-def breed
-  @breed
-end
 
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+#
+#   def name=(dogs_name)
+#     @name= dogs_name
+# end
+#
+# def name
+#   @name
+# end
+#
+# def breed= (breed)
+#   @breed= breed
+# end
+#
+# def breed
+#   @breed
+# end
+#
+# end
--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,79 @@
-require 'pry'
-
 class Person
+  def job
+    @job
+  end
 
-  def name= (person_name)
-    @name= person_name
-end
+  def job=(job)
+    @job = job
+  end
 
-def name
-  @name
-end
+  def name
+    @name
+  end
 
-def job= (new_job)
-  @job= new_job
-end
+  def name=(name)
+    @name = name
+  end         
+end # end of the Person class
 
-def job
-  @job
-end
 
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+#
+# class Person
+#
+#   def name= (person_name)
+#     @name= person_name
+# end
+#
+# def name
+#   @name
+# end
+#
+# def job= (new_job)
+#   @job= new_job
+# end
+#
+# def job
+#   @job
+# end
+#
+# end
